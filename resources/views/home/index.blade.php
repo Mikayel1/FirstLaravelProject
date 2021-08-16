a295f8193596fec62ce2462345fb8bf02865347d
Mikayel1 <mikayelarakelyan10@gmail.com>
2021-08-16 14:41:03 +0400

    COm

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -222,7 +222,7 @@
                             }
                         @endphp
 						<div class="product">
-                            <div class="product_image"><img src="images/{{$image}}" alt="{{$product->title}}"></div>df
+                            <div class="product_image"><img src="images/{{$image}}" alt="{{$product->title}}"></div>
 							<div class="product_extra product_new"><a href="categories.html">New</a></div>
 							<div class="product_content">
                                 <div class="product_title"><a href="product.html">{{$product->title}}</a></div>
